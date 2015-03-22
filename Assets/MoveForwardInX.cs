1f70f0b4568b9432e030672ca7eb7a0c726a37e8
Supaidaman <viniciuslc476@gmail.com>
2015-03-22 20:27:33 -0300

    Mais testes
    Uhul

---
+++ Assets/MoveForwardInX.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveForwardInX : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= transform.up * Time.deltaTime * 20f;
+    }
+}
